586604eef908198b0ddfb787daad6ea1513fd593
SDN33 <s.deinegri2@gmail.com>
2024-06-28 14:27:13 +0200

    sevenstore

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'home#index', as: :storefront_root
+
   mount SolidusAdmin::Engine, at: '/admin', constraints: ->(req) {
     req.cookies['solidus_admin'] != 'false' &&
     req.params['solidus_admin'] != 'false'
@@ -6,7 +8,7 @@ Rails.application.routes.draw do
   mount SolidusPaypalCommercePlatform::Engine, at: '/solidus_paypal_commerce_platform'
   scope(path: '/') { draw :storefront }
 
-  mount Spree::Core::Engine, at: '/'
+  mount Spree::Core::Engine, at: '/shop' # Changez la racine pour Spree
 
   # Add unique route names to avoid conflicts
   get '/checkout/address', to: 'checkout#address', as: 'checkout_address'
@@ -14,11 +16,10 @@ Rails.application.routes.draw do
   get '/checkout/payment', to: 'checkout#payment', as: 'checkout_payment'
   get '/checkout/confirm', to: 'checkout#confirm', as: 'checkout_confirm'
   get '/checkout/complete', to: 'checkout#complete', as: 'checkout_complete'
-  get '/list-products', to: 'products#index', as: 'list_products' # Example: Renamed to 'list_products'
+  get '/list-products', to: 'products#index', as: 'list_products'
   get '/pages/_matesclubform', to: 'pages#matesclubform', as: 'matesclubform_page'
   get 'matesclubform', to: 'pages#matesclubform', as: 'matesclubform'
   get 'contact', to: 'pages#contact', as: 'contact'
-
 
   # Other routes...
 end
